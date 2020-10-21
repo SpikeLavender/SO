651cda05434325a36452f3749bcd12962989b417
pikaqiu330 <zhangminyj@chinamobile.com>
2020-10-21 10:41:54 +0800

    fix service artifact issue
    Signed-off-by: pikaqiu330 <zhangminyj@chinamobile.com>

--- asdc-controller/src/main/java/org/onap/so/asdc/installer/heat/ToscaResourceInstaller.java
+++ asdc-controller/src/main/java/org/onap/so/asdc/installer/heat/ToscaResourceInstaller.java
@@ -2919,12 +2919,12 @@ public class ToscaResourceInstaller {
         List<ServiceArtifact> serviceArtifactList = new ArrayList<>();
         ServiceArtifact serviceArtifact;
         List<IArtifactInfo> artifactInfoList = vfResourceStruct.getNotification().getServiceArtifacts().stream()
-                .filter(artifact -> artifact.getArtifactType().equalsIgnoreCase("OTHER")).collect(Collectors.toList());
+                .filter(artifact -> artifact.getArtifactType().equalsIgnoreCase("WORKFLOW")).collect(Collectors.toList());
         for (IArtifactInfo artifactInfo : artifactInfoList) {
             serviceArtifact = new ServiceArtifact();
             serviceArtifact.setArtifactUUID(artifactInfo.getArtifactUUID());
             serviceArtifact.setName(artifactInfo.getArtifactName());
-            serviceArtifact.setType(artifactInfo.getArtifactType());
+            serviceArtifact.setType("OTHER");
             serviceArtifact.setVersion(artifactInfo.getArtifactVersion());
             serviceArtifact.setDescription(artifactInfo.getArtifactDescription());
             serviceArtifact.setChecksum(artifactInfo.getArtifactChecksum());
